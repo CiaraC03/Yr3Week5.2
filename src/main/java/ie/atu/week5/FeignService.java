3e69716161526a7cb7c51f7014a8671a1f68dba6
ciara <g00402357@atu.ie>
2023-10-31 14:31:11 +0000

    Created an instance of class TodoClient. Called the method fetchData() from TodoResponse. Prints list and if the todo has an id of 96 it extracts it. Static error here.

--- src/main/java/ie/atu/week5/FeignService.java
+++ src/main/java/ie/atu/week5/FeignService.java
@@ -1,4 +1,33 @@
 package ie.atu.week5;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FeignService {
+
+    private static TodoClient todoClient;
+
+    public FeignService(TodoClient todoClient)
+    {
+        this.todoClient = todoClient;
+    }
+
+    public static TodoResponse fetchData()
+    {
+        List<TodoResponse> td = todoClient.fetchData(); //creates a list from TodoResponse to variable td and calls the fetch data method from object todoClient
+        System.out.println(td); //prints the list from variable td
+        TodoResponse todoResponse = new TodoResponse(); //created an instance of class TodoResponse
+        for (TodoResponse todo : td) //created a loop to run through all to do objects
+        {
+            if(todo.getId() == 196) //Extract the to do with an id of 96
+            {
+                System.out.println(todo.toString());//return the to do
+
+            }
+        }
+        return todoResponse; //returning the to do
+
+    }
 }
